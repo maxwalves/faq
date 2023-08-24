1edc593d433099a4094697a81dfcaba707d0a57d
maxwalves <maxwilliamalves@gmail.com>
2023-08-23 22:44:07 -0300

    Commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,83 @@
+<html lang="en" style="height: 100%;">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>AdminLTE 3 | FAQ</title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
+      <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">      
+   </head>
+   <body class="antialiased">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('img/fundo.png') }}" alt="Logo" width="auto" height="100">
+            </a>
+        </div>
+    </nav>
+    
+
+        <div class="container" style="height: 100%;">
+            
+            <div class="content-wrapper" style="min-height: 974px;">
+                <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>FAQ</h1>
+                        </div>
+                    </div>
+                </div>
+                </section>
+                <section class="content">
+                <div class="row">
+                    @foreach($faqs as $faq)
+                        <div class="col-12" id="accordion">
+                            <div class="card card-primary card-outline">
+                                <a class="d-block w-100" data-toggle="collapse" href="#collapseOne" aria-expanded="true">
+                                <div class="card-header">
+                                    <h4 class="card-title w-100">
+                                        1. {{$faq->pergunta}}
+                                    </h4>
+                                </div>
+                                </a>
+                                <div id="collapseOne" class="collapse show" data-parent="#accordion" style="">
+                                <div class="card-body">
+                                    {{$faq->resposta}}
+                                </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                    @endforeach
+
+                    
+                </div>
+                <div class="row">
+                    <div class="col-12 mt-3 text-center">
+                        <p class="lead">
+                            <a href="contact-us.html">Entre em contato</a>,
+                            se você não encontrou a resposta para o que estava procurando.<br>
+                        </p>
+                    </div>
+                </div>
+                </section>
+            </div>
+
+            <footer class="main-footer" style="">
+                <div class="float-right d-none d-sm-block">
+                    <a href="{{ route('login') }}" class="nav-link">Administração</a>
+                </div>
+               
+            </footer>
+            
+            <div id="sidebar-overlay"></div>
+        </div>
+        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>      
+
+   </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Controllers\SistemaController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\FaqController;
+use App\Models\Faq;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $faqs = Faq::all();
+    return view('welcome', ['faqs' => $faqs]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        $user = auth()->user();
+        return view('dashboard', ['user'=> $user]);
+    })->name('dashboard');
+});
+
+
+   // ROTAS PARA ADM USERS
+   Route::get('/users/users', [UsersController::class, 'users'])->middleware('auth');
+   Route::get('/users/create', [UsersController::class, 'create'])->middleware('auth');
+   Route::get('/users/{id}', [UsersController::class, 'show'])->middleware('auth');
+   Route::delete('/users/{id}', [UsersController::class, 'destroy'])->middleware('auth');
+   Route::get('/users/edit/{id}', [UsersController::class, 'edit'])->middleware('auth');
+
+   Route::get('/users/editPerfil/{id}', [UsersController::class, 'editPerfil'])->middleware('auth');
+   Route::get('/users/editPerfil/{id}/{ordem}', [UsersController::class, 'updatePerfil'])->middleware('auth');
+
+   Route::put('/users/update/{id}', [UsersController::class, 'update'])->middleware('auth');
+   Route::post('/users', [UsersController::class,'store'])->middleware('auth');
+   Route::get('/sistema', [SistemaController::class,'sistema'])->middleware('auth');
+
+
+   Route::get('/faqs', [FaqController::class, 'index'])->middleware('auth');
+   Route::get('/faqs/create', [FaqController::class, 'create'])->middleware('auth');
+   Route::post('/faqs', [FaqController::class, 'store'])->middleware('auth');
+   Route::get('/faqs/{id}', [FaqController::class, 'show'])->middleware('auth');
+   Route::get('/faqs/edit/{id}', [FaqController::class, 'edit'])->middleware('auth');
+   Route::put('/faqs/{id}', [FaqController::class, 'update'])->middleware('auth');
+   Route::put('/updateFaq/{id}', [FaqController::class, 'updateFaq'])->middleware('auth');
+   Route::delete('/faqs/{id}', [FaqController::class, 'destroy'])->middleware('auth');
