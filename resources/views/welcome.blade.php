5d056cb459e9520bebd3e4c8ac1a5e84e9e0526e
maxwalves <maxwilliamalves@gmail.com>
2023-08-23 23:36:21 -0300

    Correção mais uma.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>AdminLTE 3 | FAQ</title>
+      <title>FAQ Jurisoft</title>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
       <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
       <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">      
